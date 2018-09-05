9e42966fe2b85586db8a3290cca20de957ea85ee
Nikita Datsenko <nikdatsenko@gmail.com>
2018-09-05 15:38:18 +0300

    add pairs, tickers for p2pb2b

---
+++ lib/cryptoexchange/exchanges/p2pb2b/market.rb
@@ -0,0 +1,8 @@
+module Cryptoexchange::Exchanges
+  module P2pb2b
+    class Market < Cryptoexchange::Models::Market
+      NAME = 'p2pb2b'
+      API_URL = 'https://p2pb2b.io/api/v1'
+    end
+  end
+end
